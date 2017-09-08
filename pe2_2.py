0468ac498e7501c8b6828bac3a48008497de8c34
Arman.K <arman.k98@live.nl>
2017-09-08 12:30:13 +0200

    ArmanK

---
+++ pe2_2.py
@@ -0,0 +1,7 @@
+cijferICOR = 9
+cijferPROG = 8
+cijferCSN = 7
+gemiddelde = (cijferICOR + cijferPROG + cijferCSN)/3
+beloning = (9+8+6) * 30
+lijn1 = "Mijn cijfers (gemiddeld een " + str(gemiddelde) + ") leveren een beloning van €" + str(beloning) + " op!"
+print(lijn1)

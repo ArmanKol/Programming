b78acd188aa33ce0a1a859b0770a545d3667aec5
Arman.K <arman.k98@live.nl>
2017-09-08 12:30:36 +0200

    ArmanK

---
+++ pe2_4.py
@@ -0,0 +1,5 @@
+uurloon = eval(input("Wat verdien je per uur: "))
+gewerkteuren = eval(input("Hoeveel uren heb je gewerkt: "))
+totaal = gewerkteuren * uurloon
+lijn1 = str(gewerkteuren) + " uur werken levert " + str(totaal) + " Euro op"
+print(lijn1)

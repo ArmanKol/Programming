dbf8897b75a98009f0de0d7098dd379eb5e5923c
Arman.K <arman.k98@live.nl>
2017-09-11 11:19:06 +0200

    ArmanK

---
+++ pe3_3.py
@@ -0,0 +1,9 @@
+age = int(input("Geef je leeftijd: "))
+paspoort = input("Nederlands paspoort: ")
+
+if age >= 18 and paspoort == "ja":
+    print("Gefeliciteerd, je mag stemmen!")
+elif age < 18 and paspoort == "ja":
+    print("Je mag niet stemmen.")
+elif age >= 18 and paspoort == "nee":
+    print("Je mag niet stemmen.")

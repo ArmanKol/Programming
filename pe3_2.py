896f6d2da2e7c1246ce82c2290e54731be3a55e1
Arman.K <arman.k98@live.nl>
2017-09-11 11:08:48 +0200

    ArmanK

--- pe3_2.py
+++ pe3_2.py
@@ -3,7 +3,3 @@ paspoort = input("Nederlands paspoort: ")
 
 if age >= 18 and paspoort == "ja":
     print("Gefeliciteerd, je mag stemmen!")
-elif age < 18:
-    print("Je bent niet oud genoeg")
-elif paspoort == "nee":
-    print("Je hebt geen Nederlands paspoort.")

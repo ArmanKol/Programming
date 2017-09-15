bd529128c981817565517ac32fbc7afa45a6ff86
Arman.K <arman.k98@live.nl>
2017-09-15 12:21:51 +0200

    ArmanK

--- pe4_3.py
+++ pe4_3.py
@@ -1,3 +1,4 @@
+#als lengtepersoon hoger of gelijk is aan 120 cm krijg je "Je bent lang....". Als lengtepersoon kleiner is dan 120 cm dan print die "Sorry je bent te klein!"
 def lang_genoeg(centimeters):
     if centimeters >= 120:
         print("Je bent lang genoeg voor de attractie!")

61ffcdffceabaf26cf318e999141b0e429c9b53e
Arman.K <arman.k98@live.nl>
2017-09-15 11:19:24 +0200

    ArmanK

--- pe4_1.py
+++ pe4_1.py
@@ -1,5 +1,10 @@
+#som neemt de getallen en telt ze vervolgens bij elkaar op. Daarna wordt het opgetelde getal teruggegeven.
 def som(getal1, getal2, getal3):
     getallen = getal1 + getal2 + getal3
     return getallen
 
-print(som(1, 2, 3))
+nummer1 = int(input("Geef getal1: "))
+nummer2 = int(input("Geef getal2: "))
+nummer3 = int(input("Geef getal3: "))
+antwoord = som(nummer1, nummer2, nummer3)
+print("Getal 1,2 en 3 worden opgegeteld en dat is: " + str(antwoord))

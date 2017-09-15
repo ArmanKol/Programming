b1dc1ad56301f3f7b15f2d0fbfd2d13d690f17cd
Arman.K <arman.k98@live.nl>
2017-09-15 13:15:20 +0200

    ArmanK

--- pe4_4.py
+++ pe4_4.py
@@ -1,7 +1,8 @@
 #als het nieuwewachtwoord aan de eisen voldoet dan print die true anders false.
 def new_password(oldpassword, newpassword):
-    newpassword1 =  oldpassword != newpassword and len(newpassword) >= 6
-    return newpassword1
+    if oldpassword != newpassword and len(newpassword) >= 6:
+        return True
+    return False
 
 password = input("Type hier je oude wachtwoord in: ")
 password2 = input("Type hier je nieuwe wachtwoord in: ")

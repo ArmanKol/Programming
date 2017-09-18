70f8a91a195b64c243e3cf1fb5b3844cabf5f710
Arman.K <arman.k98@live.nl>
2017-09-18 11:07:43 +0200

    ArmanK

--- pe3_3.py
+++ pe3_3.py
@@ -7,3 +7,5 @@ elif age < 18 and paspoort == "ja":
     print("Je mag niet stemmen.")
 elif age >= 18 and paspoort == "nee":
     print("Je mag niet stemmen.")
+elif age < 18 and paspoort == "nee":
+    print("Je mag niet stemmen.")

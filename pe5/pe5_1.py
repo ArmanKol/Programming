486db583a854c80d8e306c7f7f981fb1e669472e
Arman.K <arman.k98@live.nl>
2017-09-19 18:39:14 +0200

    ArmanK

--- pe5/pe5_1.py
+++ pe5/pe5_1.py
@@ -1,14 +1,14 @@
 import os
 import sys
-#Nog niet af.
+
 def convert(celsius):
     fahrenheit = celsius * 1.8 +32
     return fahrenheit
 
 def table():
+    print("{:12} {:5}".format("F", "C"))
     for temperatuur in range (-30, 50,10):
-        print("{:5} {:10}".format(temperatuur*1.8 +32, temperatuur))
-
-print(table())
+        print("{:5} {:10}".format(temperatuur*1.8 +32, float(temperatuur)))
+table()
 
 

1f14c233933893631cab48a46c9e1a11e4c79be2
Arman.K <arman.k98@live.nl>
2017-09-21 19:56:48 +0200

    ArmanK

---
+++ pe6/pe6_2.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+def vierletter(lijst):
+    res = []
+    for woorden in lijst:
+        if len(woorden) == 4:
+            res.append(woorden)
+    print(res)
+
+#invoer = eval(input("Geef lijst met minimaal 10 strings: "))
+lijst1 = ["boter", "kaas", "bier", "pizza","thee", "drop", "koek", "cola", "boterham", "stamppot"]
+
+
+vierletter(lijst1)

fa3ae775554888e000e869fbe94d3861ed0f1b47
Arman.K <arman.k98@live.nl>
2017-09-23 16:39:33 +0200

    ArmanK

---
+++ pe6/pe6_4.py
@@ -0,0 +1,19 @@
+studentencijfers = [[95,92,86], [66,75,54], [89,72,100], [34,0,0]]
+
+def gemiddelde_per_student(studentencijfers):
+    res = []
+    for row in studentencijfers:
+        for items in row:
+            res.append(items)
+    antw= [sum(res[0:3]) / 3, sum(res[3:6]) / 3, sum(res[6:9]) / 3, sum(res[9:12]) / 3]
+    return antw
+
+def gemiddelde_van_alle_studenten(studentencijfes):
+    res = []
+    for row in studentencijfers:
+        for items in row:
+            res.append(items)
+        antw = int(sum(res) / len(res))
+    return antw
+print(gemiddelde_per_student(studentencijfers))
+print(gemiddelde_van_alle_studenten(studentencijfers))

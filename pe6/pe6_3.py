4e5edd9e75de4826e80f69b2bf0bccba3164ef6a
Arman.K <arman.k98@live.nl>
2017-09-23 13:12:38 +0200

    ArmanK

---
+++ pe6/pe6_3.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+invoer = "5-9-7-1-7-8-3-2-4-8-7-9"
+lijst = []
+invoer = invoer.split("-")
+for cijfers in invoer:
+    if cijfers in invoer:
+        lijst.append(int(cijfers))
+print("Gesorteerde list van ints:", lijst)
+print("Grootste getal:", max(lijst), "en Kleinste getal:", min(lijst))
+print("Aantal getallen:", len(lijst), "en Som van de getallen:", sum(lijst))
+print("Gemiddelde:", sum(lijst) / len(lijst) )
+
+
+

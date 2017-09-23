776cc219847fc4a80c19377edba71c2afc922e63
Arman.K <arman.k98@live.nl>
2017-09-23 16:41:27 +0200

    ArmanK

--- pe6/pe6_4.py
+++ pe6/pe6_4.py
@@ -5,7 +5,7 @@ def gemiddelde_per_student(studentencijfers):
     for row in studentencijfers:
         for items in row:
             res.append(items)
-    antw= [sum(res[0:3]) / 3, sum(res[3:6]) / 3, sum(res[6:9]) / 3, sum(res[9:12]) / 3]
+        antw= [sum(res[0:3]) / 3, sum(res[3:6]) / 3, sum(res[6:9]) / 3, sum(res[9:12]) / 3]
     return antw
 
 def gemiddelde_van_alle_studenten(studentencijfers):

172132c012c2bac441d1e85549013445c804e2b3
Arman.K <arman.k98@live.nl>
2017-09-25 08:47:31 +0200

    ArmanK

--- pe3/pe3_1.py
+++ pe3/pe3_1.py
@@ -1,5 +1,5 @@
 score = int(input("Geef je score: "))
 if score > 15:
     print("Gefeliciteerd")
-    print("Met een score van " + str(score) + " ben je geslaagd!")
+    print("Met een score van",score,"ben je geslaagd!")
 print("Je bent niet geslaagd.")

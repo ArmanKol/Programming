06971cc6e84175e61f05315f1a4a2afdfb0b7d18
Arman.K <arman.k98@live.nl>
2017-09-25 09:24:17 +0200

    ArmanK

--- pe6/pe6_3.py
+++ pe6/pe6_3.py
@@ -7,7 +7,7 @@ invoer = invoer.split("-")
 for cijfers in invoer:
     if cijfers in invoer:
         lijst.append(int(cijfers))
-print("Gesorteerde list van ints:", lijst)
+print("Gesorteerde list van ints:", sorted(lijst))
 print("Grootste getal:", max(lijst), "en Kleinste getal:", min(lijst))
 print("Aantal getallen:", len(lijst), "en Som van de getallen:", sum(lijst))
 print("Gemiddelde:", sum(lijst) / len(lijst) )

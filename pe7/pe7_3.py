f060c5e7c09c98294161404ee85cc0dd885d6d1e
Arman.K <arman.k98@live.nl>
2017-09-26 22:53:35 +0200

    ArmanK

--- pe7/pe7_3.py
+++ pe7/pe7_3.py
@@ -2,9 +2,11 @@ import os
 import sys
 
 cursus = {"Piet":9.0, "Henk": 4.2, "Martijn":9.5, "Jonas":9.2, "Diederik":8.8, "Olaf":7, "Max":9.2, "Jolyon":1.1,"Daniel":9.1,"Mustafa":10}
+itemsCursus = cursus.items()
+valueCursus = cursus.values()
 
 def hogecijfers(cursus):
-    for cijfers in cursus.values():
-        if cijfers > 9 in cursus.values():
-            print(cursus.items(cijfers))
+    for naam,cijfer in itemsCursus:
+        if cijfer > 9.0 in valueCursus:
+            print(naam, cijfer)
 hogecijfers(cursus)

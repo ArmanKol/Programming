90f1196147df04ab02b1a737d48a1c117f119efe
Arman.K <arman.k98@live.nl>
2017-09-28 19:13:05 +0200

    ArmanK

---
+++ pe8/pe8_1.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+bruin = {"Boxtel", "Best", "Beukenlaan","Helmond't Hout", "Helmond", "Helmond Brouwhuis", "Deurne"}
+groen = {"Boxtel", "Best", "Beukenlaan", "Eindhoven", "Geldrop", "Heeze", "Weert"}
+
+print(bruin.intersection(groen))
+print(bruin.difference(groen))
+print(bruin.union(groen))

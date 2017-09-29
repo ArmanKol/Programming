f6f0534a081b73cfd4c95bd67bf213d230eea51a
Arman.K <arman.k98@live.nl>
2017-09-29 17:02:02 +0200

    ArmanK

---
+++ pe8/pe8_4.py
@@ -0,0 +1,27 @@
+tuple = ("Hallo", "Hallo", 1, 1)
+print(type(tuple))
+print(tuple)
+print()
+
+
+Set = {"Hallo", "Hallo", 1, 1, 2, 3, 4}
+print(type(Set))
+print(Set)
+Set.add("Boom")
+print(Set)
+print()
+
+
+dictionary = {"Hallo":1, "Hallo":1, "Boom":2}
+print(type(dictionary))
+print(dictionary)
+dictionary["Python"] = 1337
+print(dictionary)
+print()
+
+
+lijst = ["Hallo", "Hallo", 1, 1, "Boom", 2]
+print(type(lijst))
+print(lijst)
+lijst.append("Python")
+print(lijst)
